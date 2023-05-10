02717674e1a10e993b4ccc24d73432b4dd3f2ee5
stst2021 <124453502+stst2021@users.noreply.github.com>
2023-05-10 20:26:56 +0900

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -5,11 +5,9 @@ const closeModalBtn = document.getElementById('close-modal-btn');
 showRulesBtn.addEventListener('click', () => {
   rulesModal.style.display = 'block';
 });
-
 closeModalBtn.addEventListener('click', () => {
   rulesModal.style.display = 'none';
 });
-
 const rulesModal2 = document.getElementById('rules-modal2');
 rulesModal2.innerHTML = '<div class="rule-content"><h2>遊び方</h2><p>とても簡単!<br>注文リストにあるドリンクの合ったボタンを押せばいいだけ‼<BR>世界記録付き</p></div><button id="close-modal-btn2">閉じる</button>';
 const showRulesBtn2 = document.getElementById('show-rules-btn2');
@@ -17,7 +15,6 @@ const closeModalBtn2 = document.getElementById('close-modal-btn2');
 showRulesBtn2.addEventListener('click', () => {
   rulesModal2.style.display = 'block';
 });
-
 closeModalBtn2.addEventListener('click', () => {
   rulesModal2.style.display = 'none';
 });
